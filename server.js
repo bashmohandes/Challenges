0710996a3ff1a4f3f38bc8b820ba864e7fb54681
Mohamed Elsherif <Bashmohandes@live.com>
2017-01-25 23:04:53 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.post('/', function(req, res){
+    const hash = crypto.createHash('sha256')
+                       .update(req.body.message)
+                       .digest('hex');
+    res.json({ digest: hash});
+});
+
+router.get('/:digest', function(req, res){
+    res.json(req.params.digest);
+});
+
+app.use('/messages', router);
+
+app.listen(port);
+
+console.log('Started on port ', port);
